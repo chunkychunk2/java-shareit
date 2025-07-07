efcfe667fed06077010915a773429a95f77d6eaf
andrey.chunov <andrey.chunov@tele2.ru>
2025-07-07 21:48:23 +0300

    fix code

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -8,12 +9,14 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/items")
 @Validated
 public class ItemController {
+
+    private static final String USER_ID_HEADER = "X-Sharer-User-Id";
+
     private final ItemService service;
 
     public ItemController(ItemService service) {
@@ -22,32 +25,34 @@ public class ItemController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ItemDto create(@Valid @RequestBody ItemDto dto, @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ItemDto create(@Valid @RequestBody ItemDto dto, @RequestHeader(USER_ID_HEADER) Long userId) {
         return service.create(dto, userId);
     }
 
     @PutMapping("/{itemId}")
-    public ItemDto update(@Valid @PathVariable Long itemId, @RequestBody ItemDto dto, @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ItemDto update(@Valid @PathVariable Long itemId, @RequestBody ItemDto dto, @RequestHeader(USER_ID_HEADER) Long userId) {
         return service.update(itemId, dto, userId);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto patchUpdate(@PathVariable Long itemId, @RequestBody ItemDto dto, @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ItemDto patchUpdate(@PathVariable Long itemId, @RequestBody ItemDto dto, @RequestHeader(USER_ID_HEADER) Long userId) {
         return service.update(itemId, dto, userId);
     }
 
     @GetMapping("/{itemId}")
-    public Optional<ItemDto> getById(@PathVariable Long itemId, @RequestHeader("X-Sharer-User-Id") Long userId) {
-        return service.getById(itemId, userId);
+    public ItemDto getById(@PathVariable Long itemId, @RequestHeader(USER_ID_HEADER) Long userId) {
+        return service.getById(itemId, userId)
+                .orElseThrow(() -> new EntityNotFoundException("Вещь с id " + itemId + " не найдена"));
     }
 
+
     @GetMapping
-    public List<ItemDto> getAllByUser(@RequestHeader("X-Sharer-User-Id") Long userId) {
+    public List<ItemDto> getAllByUser(@RequestHeader(USER_ID_HEADER) Long userId) {
         return service.getAllByUser(userId);
     }
 
     @GetMapping("/search")
-    public List<ItemDto> search(@RequestParam String text, @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public List<ItemDto> search(@RequestParam String text, @RequestHeader(USER_ID_HEADER) Long userId) {
         return service.search(text);
     }
 }
